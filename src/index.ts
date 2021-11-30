144efc2d655e87d74d945483c023c21ff3cd3e9a
yigittopm <yigittopm@hotmail.com>
2021-11-30 23:07:14 +0300

    setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="@types/google.maps" />
+
+import { User } from "./User"
+import { Company } from "./Company"
+
+//const user = new User()
+const comp = new Company()
+console.log(comp)
+
